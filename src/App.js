26a6708cd0e53936019e920db42f3426708d6d85
reznye <luiz.matthew187@gmail.com>
2021-08-26 00:03:33 +0100

    start

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './components/home.js';
+
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  Link
+} from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default App;
