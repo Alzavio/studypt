125b5c4358b06425cc08ea1387a5edfb49200c60
reznye <luiz.matthew187@gmail.com>
2021-12-04 22:18:06 -0800

    language selector

--- src/components/search.js
+++ src/components/search.js
@@ -24,86 +24,94 @@ import '../css/search.css';
 
 export default function Search() {
     const [loading, setLoading] = useState(0);
+    const [language, setLanguage] = useState("Any");
     const results = useStore(state => state.results);
     console.log(results);
+    function languageSwitcher(language) {
+        console.log(language);
+        // filter the .map in the future
+        setLanguage(language);
+    }
     return (
-    <>
-        <Navibar />
-        <Container>
-            <Row className="mt-nav">
-                <Col xs={3}>
-                    <div className="bg-success position-relative" style={{marginLeft: '-10px', minHeight: '265px'}}>
-                        <div className="bg-light position-absolute w-100 px-4 py-3" style={{marginTop: '-10px', marginLeft: '10px'}}> 
-                            <h5 className="pb-1">
-                                Filters
-                            </h5>
-                            <div className="mb-2">
-                                Personal
-                            </div>
-                            <select class="form-select mb-2" aria-label="Whether relative of EU member">
-                                <option selected>Non-EU citizen</option>
-                                <option selected>CPLP citizen</option>
-                                <option selected>EU citizen</option>
-                            </select>
-                            <select class="form-select" aria-label="Whether relative of EU member">
-                                <option selected>Non-relative of EU member</option>
-                                <option selected>Relative of EU member</option>
-                            </select>
-                            <div className="my-2">
-                                Language
-                            </div>
-                            <select class="form-select" aria-label="Select language">
-                                <option selected>All</option>
-                                <option>English</option>
-                                <option>Portuguese</option>
-                                <option>French</option>
-                            </select>
-                        </div>
-                    </div>
-                </Col>
-                <Col xs={9} style={{minHeight: '20rem'}}>
-                    <InputGroup className="mb-3 shadow-sm rounded border">
-                        <FormControl
-                            placeholder="Search a degree or course..."
-                            aria-label="Search a degree"
-                            aria-describedby="searchbtn"
-                            className="p-2 border-0"
-                            name="search"
-                        />
-                        <Button variant="secondary" className="border-0 bg-white" type="submit">
-                            { loading ? 
-                            <Spinner animation="border" role="status" style={{color:'grey'}} className="d-flex align-items-center">
-                            <span className="visually-hidden">Loading...</span>
-                            </Spinner>
-                            :
-                            <FontAwesomeIcon icon={faSearch} flip="horizontal" color="grey"/>
-                            }
-                        </Button>
-                    </InputGroup>
-
-                    <Card className="shadow-sm p-2">
-                        <Row className="mx-1">
-                            <div className="p-2 rounded d-flex" style={{width:'inherit'}}>
-                                <div style={{minWidth:'7rem', backgroundImage:'url("https://upload.wikimedia.org/wikipedia/commons/5/50/Ual_600px.png")', backgroundSize:'cover', aspectRatio: '1 / 1'}} className="rounded shadow-sm">
-
+        <>
+            <Navibar />
+            <Container>
+                <form method="POST">
+                    <Row className="mt-nav">
+                        <Col xs={3}>
+                            <div className="bg-success position-relative" style={{marginLeft: '-10px', minHeight: '265px'}}>
+                                <div className="bg-light position-absolute w-100 px-4 py-3" style={{marginTop: '-10px', marginLeft: '10px'}}> 
+                                    <h5 className="pb-1">
+                                        Filters
+                                    </h5>
+                                    <div className="mb-2">
+                                        Citizenship
+                                    </div>
+                                    <select class="form-select mb-2" aria-label="Whether relative of EU member">
+                                        <option selected>Non-EU citizen</option>
+                                        <option selected>CPLP citizen</option>
+                                        <option selected>EU citizen</option>
+                                    </select>
+                                    <select class="form-select" aria-label="Whether relative of EU member">
+                                        <option selected>Non-relative of EU member</option>
+                                        <option selected>Relative of EU member</option>
+                                    </select>
+                                    <div className="my-2">
+                                        Language
+                                    </div>
+                                    <select class="form-select" aria-label="Select language" value={language}>
+                                        <option>Any</option>
+                                        <option>English</option>
+                                        <option>Portuguese</option>
+                                        <option>French</option>
+                                    </select>
                                 </div>
                             </div>
-                            <Col className="mt-2" xs={9}>
-                                <h4>Computer Science </h4>
-                                University of Braga
-                                <p className="text-muted">
-                                    Braga
-                                </p>
-                                <div className="d-flex">
-                                    <div className="bg-success text-light rounded">&nbsp;Portuguese&nbsp;</div>
-                                    <div className="bg-success text-light rounded mx-2">&nbsp;English&nbsp;</div>
-                                </div>
-                            </Col>
-                        </Row>
-                    </Card>
-                </Col>
-            </Row>
-        </Container>
-    </>
+                        </Col>
+                        <Col xs={9} style={{minHeight: '20rem'}}>
+                            <InputGroup className="mb-3 shadow-sm rounded border">
+                                <FormControl
+                                    placeholder="Search a degree or course..."
+                                    aria-label="Search a degree"
+                                    aria-describedby="searchbtn"
+                                    className="p-2 border-0"
+                                    name="search"
+                                />
+                                <Button variant="secondary" className="border-0 bg-white" type="submit">
+                                    { loading ? 
+                                    <Spinner animation="border" role="status" style={{color:'grey'}} className="d-flex align-items-center">
+                                    <span className="visually-hidden">Loading...</span>
+                                    </Spinner>
+                                    :
+                                    <FontAwesomeIcon icon={faSearch} flip="horizontal" color="grey"/>
+                                    }
+                                </Button>
+                            </InputGroup>
+
+                            <Card className="shadow-sm p-2">
+                                <Row className="mx-1">
+                                    <div className="p-2 rounded d-flex" style={{width:'inherit'}}>
+                                        <div style={{minWidth:'7rem', backgroundImage:'url("https://upload.wikimedia.org/wikipedia/commons/5/50/Ual_600px.png")', backgroundSize:'cover', aspectRatio: '1 / 1'}} className="rounded shadow-sm">
+
+                                        </div>
+                                    </div>
+                                    <Col className="mt-2" xs={9}>
+                                        <h4>Computer Science </h4>
+                                        University of Braga
+                                        <p className="text-muted">
+                                            Braga
+                                        </p>
+                                        <div className="d-flex">
+                                            <div className="bg-success text-light rounded pointer" onClick={() => languageSwitcher("Portuguese")}>&nbsp;Portuguese&nbsp;</div>
+                                            <div className="bg-success text-light rounded mx-2 pointer" onClick={() => languageSwitcher("English")}>&nbsp;English&nbsp;</div>
+                                        </div>
+                                    </Col>
+                                </Row>
+                            </Card>
+                        </Col>
+                    </Row>
+                </form>
+            </Container>
+        </>
     );
 }
