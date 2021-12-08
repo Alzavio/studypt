4d86110c040e3be6dcada8c69b15b178dc333823
reznye <luiz.matthew187@gmail.com>
2021-12-07 16:54:30 -0800

    Update navbar.js

--- src/components/microComponents/navbar.js
+++ src/components/microComponents/navbar.js
@@ -13,7 +13,7 @@ export default function Navibar() {
             <Container>
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto text-secondary" id="label"> 
-                        Developed in <Twemoji options={{ className: 'twemoji' }} style={{margin: '0 .25rem 0 .25rem'}}>🇵🇹</Twemoji> by&nbsp;<a className="text-decoration-none text-muted" href="https://www.linkedin.com/in/luiz-r-02391992/" target="_blank">Luiz</a>
+                        Developed in <Twemoji options={{ className: 'twemoji' }} style={{margin: '0 .25rem 0 .25rem'}}>🇵🇹</Twemoji> by&nbsp;<a className="text-decoration-none text-muted" href="https://www.linkedin.com/in/luizrm/" target="_blank">Luiz</a>
                     </Nav>
                     <Nav>
                         <Nav.Link className="mx-2 text-dark">
