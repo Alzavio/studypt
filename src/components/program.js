102d85495639b2e395502d18659faeac5924ae8f
reznye <luiz.matthew187@gmail.com>
2021-12-13 01:02:09 -0800

    About us default

--- src/components/program.js
+++ src/components/program.js
@@ -116,12 +116,17 @@ export default function Program() {
                         </Card>
                         <div>
                             <div className="mt-4">
-                                <h5>
-                                    About program
-                                </h5>
-                                <h5>
-                                    Nearby apartments
-                                </h5>
+                                <div className="mb-4">
+                                    <h5>
+                                        About program
+                                    </h5>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+                                </div>
+                                <div className="mb-4">
+                                    <h5>
+                                        Nearby apartments
+                                    </h5>
+                                </div>
                             </div>
                         </div>
                     </Col>
